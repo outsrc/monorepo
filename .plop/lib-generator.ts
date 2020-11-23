a379782384c23ab69e20f8f69d097fe7a9eae2b4
Ernesto Freyre <ernestofreyreg@gmail.com>
2020-11-23 11:03:34 -0500

    plop

---
+++ .plop/lib-generator.ts
@@ -0,0 +1,37 @@
+import { NodePlopAPI } from 'plop'
+
+export const libGenerator = (plop: NodePlopAPI) => {
+  const prompts = [
+    {
+      type: 'input',
+      name: 'libName',
+      message: 'Library name'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Library description'
+    }
+  ]
+  const actions = ({ libName, description }) => {
+    return [
+      {
+        type: 'copy-files',
+        data: {
+          source: 'packages/shared/functions',
+          dest: `packages/shared/${libName}`,
+          replaceInFiles: {
+            '@outsrc/functions': `@outsrc/${libName}`,
+            'Templated shared functions': description
+          }
+        }
+      }
+    ]
+  }
+
+  return {
+    description: 'Shared Module Generator',
+    prompts,
+    actions
+  }
+}
